466ba730c40c173864d468f1448f940f24ca5289
Kei Nakayama <kei.of.nakayama@gmail.com>
2016-08-28 16:50:43 +0900

    alert hello

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,15 +1,20 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import {NavController, AlertController} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController, private alertCtrl: AlertController) {
   
   }
   
   public getBarcode(): void {
-    console.log('hello');
+    let alert = this.alertCtrl.create({
+      title: 'Barcode',
+      message: 'hello',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
